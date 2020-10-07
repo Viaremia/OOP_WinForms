dd1711ea90d34ea2ab2cd5768808d04bfc81ef75
Viaremia <viaremia@gmail.com>
2020-10-07 22:16:37 +0300

    OOP

---
+++ OOP6_WindowsForms/OOP6_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace OOP6_WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.finishWorkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.downloadDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sampleQueryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificationQueryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serviceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setPathToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Group = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Surname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mark = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.reportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.dataToolStripMenuItem,
+            this.serviceToolStripMenuItem,
+            this.reportToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectDBToolStripMenuItem,
+            this.disconnectDBToolStripMenuItem,
+            this.finishWorkToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // connectDBToolStripMenuItem
+            // 
+            this.connectDBToolStripMenuItem.Name = "connectDBToolStripMenuItem";
+            this.connectDBToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.connectDBToolStripMenuItem.Text = "Connect DB";
+            this.connectDBToolStripMenuItem.Click += new System.EventHandler(this.ConnectDB_Click);
+            // 
+            // disconnectDBToolStripMenuItem
+            // 
+            this.disconnectDBToolStripMenuItem.Name = "disconnectDBToolStripMenuItem";
+            this.disconnectDBToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.disconnectDBToolStripMenuItem.Text = "Disconnect DB";
+            this.disconnectDBToolStripMenuItem.Click += new System.EventHandler(this.DisconnectDB_Click);
+            // 
+            // finishWorkToolStripMenuItem
+            // 
+            this.finishWorkToolStripMenuItem.Name = "finishWorkToolStripMenuItem";
+            this.finishWorkToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.finishWorkToolStripMenuItem.Text = "Finish work";
+            this.finishWorkToolStripMenuItem.Click += new System.EventHandler(this.FinishWork_Click);
+            // 
+            // dataToolStripMenuItem
+            // 
+            this.dataToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.downloadDataToolStripMenuItem,
+            this.sampleQueryToolStripMenuItem,
+            this.modificationQueryToolStripMenuItem});
+            this.dataToolStripMenuItem.Name = "dataToolStripMenuItem";
+            this.dataToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.dataToolStripMenuItem.Text = "Data";
+            // 
+            // downloadDataToolStripMenuItem
+            // 
+            this.downloadDataToolStripMenuItem.Name = "downloadDataToolStripMenuItem";
+            this.downloadDataToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.downloadDataToolStripMenuItem.Text = "Download data";
+            this.downloadDataToolStripMenuItem.Click += new System.EventHandler(this.DownloadDB_Click);
+            // 
+            // sampleQueryToolStripMenuItem
+            // 
+            this.sampleQueryToolStripMenuItem.Name = "sampleQueryToolStripMenuItem";
+            this.sampleQueryToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.sampleQueryToolStripMenuItem.Text = "Sample query";
+            this.sampleQueryToolStripMenuItem.Click += new System.EventHandler(this.SampleQ_Click);
+            // 
+            // modificationQueryToolStripMenuItem
+            // 
+            this.modificationQueryToolStripMenuItem.Name = "modificationQueryToolStripMenuItem";
+            this.modificationQueryToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.modificationQueryToolStripMenuItem.Text = "Modification query";
+            this.modificationQueryToolStripMenuItem.Click += new System.EventHandler(this.ModificationQ_Click);
+            // 
+            // serviceToolStripMenuItem
+            // 
+            this.serviceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setPathToolStripMenuItem});
+            this.serviceToolStripMenuItem.Name = "serviceToolStripMenuItem";
+            this.serviceToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.serviceToolStripMenuItem.Text = "Service";
+            // 
+            // setPathToolStripMenuItem
+            // 
+            this.setPathToolStripMenuItem.Name = "setPathToolStripMenuItem";
+            this.setPathToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.setPathToolStripMenuItem.Text = "Set path";
+            this.setPathToolStripMenuItem.Click += new System.EventHandler(this.SetPath_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.About_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Group,
+            this.Surname,
+            this.Subject,
+            this.Mark});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 313);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "ID";
+            this.Id.Name = "Id";
+            // 
+            // Group
+            // 
+            this.Group.HeaderText = "Group";
+            this.Group.Name = "Group";
+            // 
+            // Surname
+            // 
+            this.Surname.HeaderText = "Surname";
+            this.Surname.Name = "Surname";
+            // 
+            // Subject
+            // 
+            this.Subject.HeaderText = "Subject";
+            this.Subject.Name = "Subject";
+            // 
+            // Mark
+            // 
+            this.Mark.HeaderText = "Mark";
+            this.Mark.Name = "Mark";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 380);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(775, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(752, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Grid";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(713, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Query field";
+            // 
+            // reportToolStripMenuItem
+            // 
+            this.reportToolStripMenuItem.Name = "reportToolStripMenuItem";
+            this.reportToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.reportToolStripMenuItem.Text = "Report";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 414);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectDBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectDBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem finishWorkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem downloadDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sampleQueryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificationQueryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem serviceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setPathToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Group;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Surname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Subject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mark;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolStripMenuItem reportToolStripMenuItem;
+    }
+}
+
---
+++ OOP6_WindowsForms/OOP6_WindowsForms/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP6_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        private string path = null;
+        public static string connectingString = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source =";
+        private OleDbConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            toolTip1.SetToolTip(textBox1, "The samples of query:\n"+
+                "SELECT * FROM [Session] WHERE [Mark] = 5\n" +
+                "UPDATE [Session] SET [Group] = 'ASI', [Mark] = '1' WHERE [ID] = 9 ");
+        }
+        private void About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OOP6_WinForms\nAutor: Peremena Dmitry\nGroup: IPZ-18-2", "About");
+        }
+        //Data
+        private void ModificationQ_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OleDbCommand command = new OleDbCommand(textBox1.Text, connection)) 
+                {
+                    command.ExecuteNonQuery();
+                }
+                using (OleDbCommand command = new OleDbCommand("SELECT * FROM [Session]", connection))
+                {
+                    OleDbDataReader reader = command.ExecuteReader();
+                    dataGridView1.Rows.Clear();
+                    while (reader.Read())
+                        dataGridView1.Rows.Add(reader[0], reader[1], reader[2], reader[3], reader[4]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning!");
+            }
+        }
+
+        private void SampleQ_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OleDbCommand command = new OleDbCommand(textBox1.Text, connection))
+                {
+                    dataGridView1.Rows.Clear();
+                    OleDbDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                        dataGridView1.Rows.Add(reader[0], reader[1], reader[2], reader[3], reader[4]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning!");
+            }
+        }
+
+        private void DownloadDB_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM [Session]";
+            try
+            {
+                using (OleDbCommand command = new OleDbCommand(query, connection))
+                {
+                    OleDbDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                        dataGridView1.Rows.Add(reader[0], reader[1], reader[2], reader[3], reader[4]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning!");
+            }
+        }
+
+        //File
+        private void FinishWork_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void DisconnectDB_Click(object sender, EventArgs e)
+        {
+            connection.Close();
+            dataGridView1.Rows.Clear();
+        }
+
+        private void ConnectDB_Click(object sender, EventArgs e)
+        {
+            if (path != null)
+            {
+                connection = new OleDbConnection(connectingString + path);
+                connection.Open();
+            }
+            else
+                MessageBox.Show("Set database path", "Warning!");
+        }
+        //Service
+        private void SetPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "DB file(*.mdb)|*.mdb";
+            if (openFile.ShowDialog() == DialogResult.Cancel)
+                return;
+            else
+                path = openFile.FileName;
+        }
+    }
+}
