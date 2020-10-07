b41557b6f55fda4133d21a0a114f89e19fb34fd4
Viaremia <viaremia@gmail.com>
2020-10-07 22:20:19 +0300

    OOP

---
+++ OOP7_WindowsForms/OOP7_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace OOP7_WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.group = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.surname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mark = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(25, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 201);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(431, 210);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(191, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(255, 136);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 77);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(170, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.group,
+            this.surname,
+            this.subject,
+            this.mark});
+            this.dataGridView1.Location = new System.Drawing.Point(452, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(452, 378);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.Width = 50;
+            // 
+            // group
+            // 
+            this.group.HeaderText = "Group";
+            this.group.Name = "group";
+            // 
+            // surname
+            // 
+            this.surname.HeaderText = "Surname";
+            this.surname.Name = "surname";
+            // 
+            // subject
+            // 
+            this.subject.HeaderText = "Subject";
+            this.subject.Name = "subject";
+            // 
+            // mark
+            // 
+            this.mark.HeaderText = "Mark";
+            this.mark.Name = "mark";
+            this.mark.Width = 60;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(25, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(25, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Text space";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(206, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Image";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(475, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Data grid";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(848, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(777, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 18);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Status";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Accept";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Accept_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(916, 423);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Server_Closing);
+            this.Load += new System.EventHandler(this.Server_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn group;
+        private System.Windows.Forms.DataGridViewTextBoxColumn surname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn subject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mark;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ OOP7_WindowsForms/OOP7_WindowsForms/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace OOP7_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        private const string IPAddr = "127.0.0.1";
+        private static IPEndPoint IPEndPoint;
+        private static ManualResetEvent allDone = new ManualResetEvent(false);
+        Socket server;
+        Socket handler;
+        //Socket client;
+        //TcpListener listener;
+
+        string Query = "SELECT * FROM [Session]";
+        string connectingString = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = Session.mdb";
+        OleDbConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Server_Load(object sender, EventArgs e)
+        {
+                DataGrid_Load();
+        }
+
+        private async void Accept_Click(object sender, EventArgs e)
+        {
+            string data = null;
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+                MessageBox.Show("Some of the fields are not write in.", "Warning!");
+            else
+            {
+                //listener = new TcpListener(IPAddress.Parse(IPAddr), int.Parse(textBox1.Text));
+
+                //listener.Start();
+                //try
+                //{
+                //    while (true)
+                //    {
+                //        //allDone.Reset();
+
+                //        client = await listener.AcceptSocketAsync(); //AcceptSocket();
+                //        while (client.Connected)
+                //        {
+                //            byte[] bytes = new byte[1024];
+                //            int bytesRec = client.Receive(bytes);
+                //            data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                //        }
+                //        //allDone.WaitOne();
+
+                //        //client.Close();
+                //    }
+                //}
+                //catch (Exception ex)
+                //{
+                //    MessageBox.Show(ex.Message);
+                //}
+
+
+                try
+                {
+                    IPEndPoint = new IPEndPoint(IPAddress.Parse(IPAddr), int.Parse(textBox1.Text));
+                    server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    server.Bind(IPEndPoint);
+                    server.Listen(2);
+
+
+
+                    while (true)
+                    {
+                        try
+                        {
+                            await Task.Run(() =>
+                            {
+                                handler = server.Accept();
+                            });
+
+
+                            StringBuilder builder = new StringBuilder();
+                            byte[] bytes = new byte[1024];
+                            //int bytesRec = server.Receive(bytes);
+                            do
+                            {
+                                int bytesRec = handler.Receive(bytes);
+                                builder.Append(Encoding.UTF8.GetString(bytes, 0, bytesRec));
+                                //data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                                MessageBox.Show("Connect", "Message!");
+                            }
+                            while(handler.Available > 0);
+                            
+
+
+                            //while (handler.Connected)
+                            //{
+                            //    bytesRec = handler.Receive(bytes);
+                            //    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                            //    MessageBox.Show("Sended", "Message");
+                            //}
+                            //handler.Shutdown(SocketShutdown.Both);
+                            //handler.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                    //await Task.Run(() => 
+                    //{
+                    //    while (true)
+                    //    {
+                    //        byte[] bytes = new byte[1024];
+
+                    //        int bytesRec = handler.Receive(bytes);
+                    //        data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                    //        MessageBox.Show("Done", "Message");
+                    //    }
+
+                    //});
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        //private async void Accept()
+        //{
+        //    for(; ; )
+        //    {
+        //        var handler = await Task.Factory.FromAsync<Socket>(server.BeginAccept, server.EndAccept, true);
+        //        MessageBox.Show("Connected", "Message!");
+        //    }
+        //}
+
+        private void DataGrid_Load()
+        {
+            try
+            {
+                connection = new OleDbConnection(connectingString);
+                connection.Open();
+                using (OleDbCommand command = new OleDbCommand(Query, connection))
+                {
+                    OleDbDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                        dataGridView1.Rows.Add(reader[0], reader[1], reader[2], reader[3], reader[4]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Server_Closing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                //server.Shutdown(SocketShutdown.Both);
+                //handler.Shutdown(SocketShutdown.Both);
+                //handler.Close();
+                //server.Close();
+                //client.Close();
+                //listener.Stop();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
